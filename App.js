3c0939178dc9c64b2d37b4d28fa1bcca1aaf6962
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-12-06 17:33:25 +0700

    functional component

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import {Text} from 'react-native';
+import React from 'react';
+import {View, Text} from 'react-native';
 
-export default class App extends Component {
-
-  render(){
-    return <Text>Hello Dumbways.id</Text>
-  }
-}
+export default ()=> (
+  <View>
+    <Text>Hello Dumbways.id on functional Component</Text>
+    <Text>Another Text</Text>
+  </View>
+)
